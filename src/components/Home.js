8766d06d718fec55b7810f9de189ae200b7ca214
Rizo <g.rizzoli@hotmail.ca>
2023-03-15 00:49:37 -0400

    page d'accueil

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+const Home = () => {
+    return (
+        <div>
+            <h4>Where's the target?!</h4>
+        </div>
+    )
+}
+
+export default Home
