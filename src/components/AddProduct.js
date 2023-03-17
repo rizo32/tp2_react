382ec05d5ecb1e49b0ef9828e944cc993ff76fc8
Rizo <g.rizzoli@hotmail.ca>
2023-03-16 22:57:15 -0400

    ajout de la fonction d'ajout de produits

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,54 +1,96 @@
 import { useState } from "react";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import { FaArrowLeft, FaCheck } from "react-icons/fa";
 
-const AddProduct = ({ onAdd }) => {
-	const [name, setName] = useState("");
-	const [description, setDescription] = useState("");
-	const [price, setPrice] = useState("");
-	const [category, setCategory] = useState("");
+const AddProduct = ({ onProductAdd }) => {
+	// Initialisation du produit
+	const id = Math.floor(Math.random() * 1000);
+	const [newProduct, setNewProduct] = useState({
+		id: id,
+		name: "",
+		price: "",
+		description: "",
+		category: "",
+	});
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		if (!name) {
-			alert("Where's the product?");
-			return;
-		}
-		onAdd({ name, description, price, category });
-		setName("");
-		setDescription("");
-		setPrice("");
-		setCategory("");
+	const handleValueChange = (event) => {
+		const { name, value } = event.target;
+		setNewProduct((prevProduct) => ({
+			...prevProduct,
+			[name]: value,
+		}));
 	};
+
+	const navigate = useNavigate();
+
+	// Gestion de la soumission du formulaire
+	const onSubmit = (event) => {
+		event.preventDefault();
+		onProductAdd(newProduct);
+		setNewProduct({
+			id: "",
+			name: "",
+			price: "",
+			description: "",
+			category: "",
+		});
+		navigate("/products");
+	};
+
 	return (
-		<div className="container text-center pt-4">
-			<h3>Ajout dans l'inventaire</h3>
-			<form onSubmit={onSubmit} className="d-flex flex-column gap-2 pt-2">
+		<div className="container py-1 d-flex flex-column gap-5 w-50">
+			<form onSubmit={onSubmit} className="bg-light container border border-info rounded py-4">
+			<h3 className="text-center">Ajout dans l'inventaire</h3>
 				<SearchBar
 					type="text"
-					placeholder="What's my name?"
+					value={newProduct.name}
+					onChange={handleValueChange}
+					placeholder="ex. thingy"
 					name="name"
 					label="Name"
+          isRequired="true"
 				/>
 				<SearchBar
 					type="text"
-					placeholder="What's my price?"
+					value={newProduct.price}
+					onChange={handleValueChange}
+					placeholder="ex. 99.99$"
 					name="price"
-					label="price"
+					label="Price"
+          isRequired="true"
 				/>
 				<SearchBar
 					type="text"
-					placeholder="What's my description?"
+					value={newProduct.description}
+					onChange={handleValueChange}
+					placeholder="ex. blabla"
 					name="description"
 					label="Description"
 				/>
 				<SearchBar
 					type="text"
-					placeholder="What's my category?"
+					value={newProduct.category}
+					onChange={handleValueChange}
+					placeholder="ex. Laptop"
 					name="category"
 					label="Category"
+          isRequired="true"
 				/>
-				<input type="submit" value="Submit" />
+				<div className="d-flex justify-content-evenly py-3">
+					<div className="lead pointer">
+						<Link
+							className="text-dark text-decoration-none"
+							to={`/products`}
+						>
+							<span>Back </span>
+							<FaArrowLeft />
+						</Link>
+					</div>
+					<input className="lead" type="submit" value="Create" />
+				</div>
 			</form>
 		</div>
 	);
