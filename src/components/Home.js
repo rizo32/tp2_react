a1580fefa9e82b391d84ebe6086ca3cf3b04ed64
Rizo <g.rizzoli@hotmail.ca>
2023-03-16 22:58:19 -0400

    ajout d'une description de l'entreprise

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <div>
-            <h4>Where's the target?!</h4>
+        <div className="container text-center">
+            <h4 className="fst-italic">Here to stay</h4>
+            <p className="py-3">Target Canada is a leading electronics retailer in the Canadian market, providing customers with a wide range of high-quality electronic products and accessories. Our mission is to provide innovative, cutting-edge technology to our customers, while delivering exceptional service and value. Our selection includes the latest smartphones, laptops, tablets, gaming consoles, home entertainment systems, and more, from top brands in the industry. Our knowledgeable staff are committed to providing customers with expert advice and support, to help them find the perfect product for their needs. Shop with us today and experience the Target Canada difference!</p>
         </div>
     )
 }
